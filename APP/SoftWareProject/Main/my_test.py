a78c7d4ca2ab0e1497f3437e6b2fcaf985b7cba5
Sepideh M <Sepideh M sepideh_m2006@yahoo.co.uk>
2016-03-02 09:42:18 +0330

    Run :D

--- APP/SoftWareProject/Main/my_test.py
+++ APP/SoftWareProject/Main/my_test.py
@@ -2,41 +2,44 @@ __author__ = 'sepideh'
 
 
 import openpyxl
+from operator import itemgetter
 
-
-def update_information_popularity_on_year(ws, name_city, year, num):
+def update_information_popularity_on_year(ws, year):
     name_city_col = 3
+    name_city_row = 29
     year_start_col = 6
     year_row = 17
+    list_pop = []
 
-    i = 18
+    i = 0
     while True:
-        if ws.cell(row=i, column=name_city_col) == name_city:
+        if ws.cell(row=year_row, column=year_start_col+i).value == year:
             j = 0
             while True:
-                if ws.cell(row=year_row, column=year_start_col+j) == year:
-                    ws.cell(row=i, column=year_start_col+j).value = num
-                    return 'update data'
-
-                elif not ws.cell(row=i, column=year_start_col+j).value:
+                if not ws.cell(row=name_city_row+j, column=name_city_col).value:
                     break
+
+                else:
+                    list_pop.append([ws.cell(row=name_city_row+j, column=name_city_col).value,
+                                    int(ws.cell(row=name_city_row+j, column=year_start_col+i).value)])
 
                 j += 1
 
-            return 'this year not exist -> year = '+year
+            list_pop
+            return list_pop
 
-        elif not ws.cell(row=i, column=name_city_col).value:
+        elif not ws.cell(row=year_row, column=year_start_col+i).value:
             break
 
         i += 1
-        print(i)
-    return 'this city not exist -> city = '+name_city
+
+    return None
 
 
 
 
 wb = openpyxl.load_workbook('..\..\..\Data\WPP2015_POP_F01_2_TOTAL_POPULATION_MALE.xlsx')
 ws = wb['ESTIMATES']
-print(update_information_popularity_on_year(wb['ESTIMATES'],'kkkk',10,1))
+print(update_information_popularity_on_year(wb['ESTIMATES'], '1950'))
 
 
